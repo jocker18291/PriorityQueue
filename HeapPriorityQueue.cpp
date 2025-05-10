704eaf7e66c6d6497c41ad7bf5a2effc46977a6d
jocker18291 <dawidans714@gmail.com>
2025-05-10 17:45:50 +0200

    Initialized heapify method

--- HeapPriorityQueue.cpp
+++ HeapPriorityQueue.cpp
@@ -5,3 +5,26 @@
 heapQueue::heapQueue(std::vector<std::pair<double, double>> array) {
     arr = array;
 };
+
+void heapQueue::heapify(std::vector<std::pair<double, double>>& arr, int n, int i) {
+
+    int largest = i;
+
+    int left = 2 * i + 1;
+
+    int right = 2 * 1 + 2;
+
+    if(arr[largest] < arr[left] && left < n) {
+        largest = left;
+    }
+
+    if(arr[largest] < arr[right] && right < n) {
+        largest = right;
+    }
+
+    if(arr[i] != arr[largest]) {
+        std::swap(arr[i], arr[largest]);
+
+        heapify(arr, n, largest);
+    }
+}
