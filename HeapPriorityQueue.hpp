8111e9e1565025cf49e35f0699cd83c3153701be
jocker18291 <dawidans714@gmail.com>
2025-05-10 16:59:20 +0200

    Add new methods and variables

--- HeapPriorityQueue.hpp
+++ HeapPriorityQueue.hpp
@@ -1,10 +1,15 @@
 #ifndef HEAPPRIORITYQUEUE_HPP
 #define HEAPPRIORITYQUEUE_HPP
+#include <vector>
 
-class heapPriority {
+class heapQueue {
 private:
     double element;
     double priority;
+    std::vector<std::pair<double, double>> arr;
+public:
+    heapQueue(std::vector<std::pair<double, double>> arr);
+    void heapify(std::vector<double>& arr, int n, int i);
 };
 
 #endif
