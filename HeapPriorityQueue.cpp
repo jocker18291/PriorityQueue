4655114415f13a2ec2ecd7ca888956b6944a0712
jocker18291 <dawidans714@gmail.com>
2025-05-10 22:25:23 +0200

    Improved heapify and add insert method

--- HeapPriorityQueue.cpp
+++ HeapPriorityQueue.cpp
@@ -6,16 +6,20 @@ heapQueue::heapQueue(std::vector<std::pair<double, double>> array) {
     arr = array;
 };
 
-void heapQueue::heapifyUp(std::vector<std::pair<double, double>>& arr, int i) {
+void heapQueue::heapifyUp(int i) {
     if(i > 0) {
         int parent = (i - 1) / 2;
 
         if(arr[parent].second < arr[i].second) {
         std::swap(arr[i], arr[parent]);
 
-        heapifyUp(arr, parent);
+        heapifyUp(parent);
         }
     }
-    
 }
 
+void heapQueue::insert(double e, double p) {
+    std::pair<double, double> newElem = {e, p};
+    arr.push_back(newElem);
+    heapifyUp(arr.size() - 1);
+}
