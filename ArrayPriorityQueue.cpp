edce58b2b906756f5cf61eb6609fa34c37025ccd
MesnerH <hubertimssar04@gmail.com>
2025-05-12 16:17:00 +0200

    Added more functions

--- ArrayPriorityQueue.cpp
+++ ArrayPriorityQueue.cpp
@@ -22,3 +22,35 @@ std::pair<double, double> arrayQueue::extract_max()
     arr.erase(arr.begin() + max_Index);
     return max_Elem;
 }
+
+std::pair<double, double> arrayQueue::find_max() const
+{
+
+    int max_Index = 0;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        if (arr[i].second > arr[max_Index].second)
+        {
+            max_Index = i;
+        }
+    }
+
+    return arr[max_Index];
+}
+
+void arrayQueue::modify_key(double e, double p)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i].first == e)
+        {
+            arr[i].second = p;
+            break;
+        }
+    }
+}
+
+int arrayQueue::return_size() const
+{
+    return arr.size();
+}
