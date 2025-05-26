d961535ec396cc61e2a06d6439627bb359a22515
jocker18291 <dawidans714@gmail.com>
2025-05-26 18:57:37 +0200

    Add initialization of methods and changed to dynamic array. All in one due to laptop error

--- HeapPriorityQueue.cpp
+++ HeapPriorityQueue.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 #include "HeapPriorityQueue.hpp"
 
-heapQueue::heapQueue() {};
+heapQueue::heapQueue() {
+    capacity = 10;
+    size = 0;
+    arr = new std::pair<double, double>[capacity];
+};
 
 void heapQueue::heapifyUp(int i) {
     if(i > 0) {
@@ -16,39 +20,52 @@ void heapQueue::heapifyUp(int i) {
     }
 }
 
-void heapQueue::heapifyDown(int n, int i) {
+void heapQueue::heapifyDown(int i) {
     int largest = i;
 
     int left = 2 * i + 1;
 
     int right = 2 * i + 2;
 
-    if(left < n && arr[largest].second < arr[left].second) {
+    if(left < size && arr[largest].second < arr[left].second) {
         largest = left;
     }
 
-    if(right < n && arr[largest].second < arr[right].second) {
+    if(right < size && arr[largest].second < arr[right].second) {
         largest = right;
     }
 
     if(largest != i) {
         std::swap(arr[i], arr[largest]);
 
-        heapifyDown(n, largest);
+        heapifyDown(largest);
+    }
+}
+
+void heapQueue::resize() {
+    if(size == capacity) {
+        std::pair<double, double>* NewArr = new std::pair<double, double>[capacity * 2];
+        capacity *= 2;
+        for(int i = 0; i < size; i++) {
+            NewArr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = NewArr;
     }
 }
 
 void heapQueue::insert(double e, double p) {
-    std::pair<double, double> newElem = {e, p};
-    arr.push_back(newElem);
-    heapifyUp(arr.size() - 1);
+    if(size == capacity) resize();
+    arr[size] = {e, p};
+    heapifyUp(size);
+    size++;
 }
 
 std::pair<double, double> heapQueue::extract_max() {
     std::pair<double, double> firstElem = arr[0];
-    std::swap(arr[0], arr[arr.size() - 1]);
-    arr.pop_back();
-    heapifyDown(arr.size(), 0);
+    std::swap(arr[0], arr[size - 1]);
+    size--;
+    heapifyDown(0);
     return firstElem;
 }
 
@@ -58,12 +75,12 @@ std::pair<double, double> heapQueue::find_max() const {
 
 void heapQueue::modify_key(double e, double p) {
     double currentP = 0;
-    for(int i = 0; i < arr.size(); i++) {
+    for(int i = 0; i < size; i++) {
         if(arr[i].first == e) {
             currentP = arr[i].second;
             arr[i].second = p;
             if(currentP > p) {
-                heapifyDown(arr.size(), i);
+                heapifyDown(i);
             }
             else if (currentP < p) {
                 heapifyUp(i);
@@ -74,6 +91,12 @@ void heapQueue::modify_key(double e, double p) {
     
 }
 
+
+
+heapQueue::~heapQueue() {
+    delete[] arr;
+}
+
 int heapQueue::return_size() const {
-    return arr.size();
+    return size;
 }
